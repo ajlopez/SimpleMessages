4dac97d67f65ffc148216567da7c413adc491f3f
ajlopez <webmaster@ajlopez.com>
2012-06-04 02:08:45 -0300

    New tests for client reading messages

--- test/createClient.js
+++ test/createClient.js
@@ -24,7 +24,7 @@ exports['Connect to Server and Write Message'] = function(test) {
     });
 }
 
-exports['Connect  to Server and Write Ten Messages'] = function(test) {
+exports['Connect to Server and Write Ten Messages'] = function(test) {
     test.expect(30);
     
     var nmsg = 0;
@@ -55,3 +55,51 @@ exports['Connect  to Server and Write Ten Messages'] = function(test) {
     });
 }
 
+exports['Connect to Server and Receive Message'] = function(test) {
+    test.expect(3);
+    
+	var server = simplemessages.createServer(function(channel) {
+        test.ok(channel);
+        channel.write({ name: "test"});
+	});
+	
+	server.listen(4000, 'localhost');
+	
+	var client = simplemessages.createClient(4000, 'localhost');
+    
+    client.on('message', function(msg) {
+        test.ok(msg);
+        test.equal(msg.name, "test");
+        test.done();
+        client.end();
+        server.close();
+    });
+}
+
+exports['Connect to Server and Receive Ten Messages'] = function(test) {
+    test.expect(31);
+    
+	var server = simplemessages.createServer(function(channel) {
+        test.ok(channel);
+        for (var k = 1; k <= 10; k++)
+            channel.write({ name: "test", number: k });
+	});
+	
+	server.listen(4000, 'localhost');
+	
+	var client = simplemessages.createClient(4000, 'localhost');
+    var nmsg = 0;
+    
+    client.on('message', function(msg) {
+        test.ok(msg);
+        test.equal(msg.name, 'test');
+        nmsg++;
+        test.equal(msg.number, nmsg);
+        
+        if (nmsg == 10) {
+            test.done();                
+            client.end();
+            server.close();
+        }
+    });
+}
